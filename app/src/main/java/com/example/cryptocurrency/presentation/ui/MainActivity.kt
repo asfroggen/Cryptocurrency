084a92bd01e27880880e5b3916415a72afba59dd
Esau <yosoypawn@hotmail.com>
2021-09-06 21:55:02 -0500

    first commit

---
+++ app/src/main/java/com/example/cryptocurrency/presentation/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.cryptocurrency.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrency.presentation.coin_detail.components.CoinDetailScreen
+import com.example.cryptocurrency.presentation.coin_list.components.CoinListScreen
+import com.example.cryptocurrency.presentation.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController = navController)
+                        }
+
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
